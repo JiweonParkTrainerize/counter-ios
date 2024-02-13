3d9a91423364e3af7f1136b6d3f9d9f1e7ada42e
Jiweon Park <jiweon@trainerize.com>
2024-02-13 12:18:10 -0800

    first commit

--- App.js
+++ App.js
@@ -1,6 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NativeModules, NativeEventEmitter } from 'react-native'
+
+const CounterEvents = new NativeEventEmitter(NativeModules.Counter)
+// subscribe to event
+CounterEvents.addListener(
+  "onIncrement",
+  res => console.log("onIncrement event", res)
+)
+console.log(NativeModules.Counter);
+NativeModules.Counter.increment()
+NativeModules.Counter.getCount(value => {
+  console.log("count is " + value)
+});
+
+
+async function decrement() {
+  try {
+    const res = await NativeModules.Counter.decrement()
+    console.log(res)
+  } catch(e) {
+    console.log(e.message, e.code)
+  }
+}
+
+decrement()
+decrement()
+
 export default function App() {
   return (
     <View style={styles.container}>
